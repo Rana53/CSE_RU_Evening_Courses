0b07941aa5257b6de3432a88fbf328b7eacba1c3
Rana53 <ranajahid.cseru@gmail.com>
2018-04-28 01:56:33 +0600

    added routes for available courses  js file

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const availableCoursesRoutes = require('./Resource/Available_Courses');
 app.use((req,res,next)=> {
     res.status().jeson({
         message: 'evening courses / available_courses'
 
     });
 });
+
+module.exports = app;
+
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/product');
+
+app.use('/products',productRoutes);
 
 module.exports = app;
