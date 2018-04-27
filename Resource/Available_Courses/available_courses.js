a70132000a7df8e698f54a876fb2af311004931d
Rana53 <ranajahid.cseru@gmail.com>
2018-04-28 02:05:18 +0600

    get method added, it's  just show simple message for testing get request.

---
+++ Resource/Available_Courses/available_courses.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+module.exports = router;
+
+ router.get('/',(req,res,next) => {
+     res.status(200).json({
+         message : 'Test get request for available_courses end point';
+     });
+ });
+
+ 
