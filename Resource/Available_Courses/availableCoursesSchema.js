d360c1bc28a0aa2a0e7e67aee5d263a44af3689a
Rana53 <ranajahid.cseru@gmail.com>
2018-05-28 14:59:50 +0600

    desing course schema in availableCourseSchema.js file

---
+++ Resource/Available_Courses/availableCoursesSchema.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const availableCourseSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    courseName : {type:String,require:true},
+    courseOfferingDept : {type: string, require: true};
+
+});
+
+module.exports = mongoose.model('AvailableCourse',availableCourseSchema);
