d7e7aaa3a2579d2cb43255792bbe6daee19ff511
Rana53 <ranajahid.cseru@gmail.com>
2018-05-28 12:31:15 +0600

    rename available_course.js file to availableCourse.js file.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const availableCoursesRoutes = require('./Resource/Available_Courses/available_courses');
+const availableCoursesRoutes = require('./Resource/Available_Courses/availableCourses');
 app.use('/EveningCourses/AvailableCourses',availableCoursesRoutes);
 
 module.exports = app;
