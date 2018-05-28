be2e56ac44de43e5a4d561abf1de5db51b141509
Rana53 <ranajahid.cseru@gmail.com>
2018-05-28 15:22:05 +0600

    available course post request successfully modefyed.

--- Resource/Available_Courses/availableCourses.js
+++ Resource/Available_Courses/availableCourses.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const AvailableCourses = require('../Available_Courses/availableCoursesSchema');
+
 module.exports = router;
 
 mongoose.connect('mongodb://dbuser:dbpassword@ds137720.mlab.com:37720/se_ru_evening_courses',err =>{
@@ -20,6 +22,34 @@ mongoose.connect('mongodb://dbuser:dbpassword@ds137720.mlab.com:37720/se_ru_even
  });
 
 router.post('/',(req,res,next) => {
+    const availableCourses = new AvailableCourses({
+        _id: new mongoose.Types.ObjectId,
+        courseName: req.body.courseName,
+        courseOfferingDept: req.body.courseOfferingDept
+    });
+    availableCourses
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: 'POST request handled',
+            createdCourse: {
+                _id: result._id,
+                courseName: result.courseName,
+                courseOfferingDept: result.courseOfferingDept
+            },
+            request: {
+                type: 'POST',
+                url: 'http://localhost:3000/Available_Courses'
+            }
+        });
+    })
+    .catch(err =>{
+        res.status(500).json({
+            message: 'Course can are not stored',
+            error: err
+        });
+    });
     res.status(200).json({
         message : 'test post request fro available_courses endpoint'
     });
--- Resource/Available_Courses/availableCoursesSchema.js
+++ Resource/Available_Courses/availableCoursesSchema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const availableCourseSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     courseName : {type:String,require:true},
-    courseOfferingDept : {type: string, require: true}
+    courseOfferingDept : {type: String, require: true}
 
 });
 
