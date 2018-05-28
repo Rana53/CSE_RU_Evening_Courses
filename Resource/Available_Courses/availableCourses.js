811d7e86ac70b5eb3909365ebb5a1fcdc45f394e
Rana53 <ranajahid.cseru@gmail.com>
2018-05-28 12:56:38 +0600

    database connection are successfully connected with mlab.com dataserver

--- Resource/Available_Courses/availableCourses.js
+++ Resource/Available_Courses/availableCourses.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 module.exports = router;
+
+mongoose.connect('mongodb://dbuser:dbpassword@ds137720.mlab.com:37720/se_ru_evening_courses',err =>{
+           // in the database link unfortunately cse repace with se cse-> se_ru_evening_course
+    if(err){
+        console.log('Database not connected error is : ' + err);
+    }
+    else{
+        console.log('Database connected');
+    }
+}); 
 
  router.get('/',(req,res,next) => {
      res.status(200).json({
