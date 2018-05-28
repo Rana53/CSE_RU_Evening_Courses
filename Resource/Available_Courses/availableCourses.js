76fda9cfe53967f4a0ae46ec4d5664751c512dd2
Rana53 <ranajahid.cseru@gmail.com>
2018-05-29 02:26:54 +0600

    another get route added  for specefic id which will find available course with it's id.

--- Resource/Available_Courses/availableCourses.js
+++ Resource/Available_Courses/availableCourses.js
@@ -40,7 +40,24 @@ router.get("/", (req,res,next) => {
         });
     });
  });
-
+router.get("/:courseId",(req,res,next) =>{
+    const id = req.param.courseId;
+    AvailableCourses.findById(id)
+    .exec()
+    .then(doc =>{
+        console.log("From database ",doc);
+        if(doc){
+            res.status(200).json(doc);
+        }
+        else{
+            res.status(404).json({message: 'not valid id'});
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({error: err});
+    });
+});
 router.post("/",(req,res,next) => {
     const availableCourses = new AvailableCourses({
         _id: new mongoose.Types.ObjectId,
